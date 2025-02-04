26351a275de41225f64c0f3d32e04ffdbcfdf744
Mamadou korka Diallo <mamadoukorkad18@gmail.com>
2025-02-04 13:35:28 +0000

    ajout de routes pour les  methodes getClient et getClientByID

--- src/routes/clientRoute.js
+++ src/routes/clientRoute.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { addClient } from "../controllers/clientController.js";
+import {
+  addClient,
+  getClient,
+  getClientByID,
+} from "../controllers/clientController.js";
 
 const router = express.Router();
 
 router.post("/add", addClient);
+router.get("/all", getClient);
+router.get("/:id", getClientByID);
 
 export default router;
