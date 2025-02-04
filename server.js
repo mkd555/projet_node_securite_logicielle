2e152e9009e5272be79d9ae948ea0621a17277ff
Mamadou korka Diallo <mamadoukorkad18@gmail.com>
2025-02-04 13:32:09 +0000

    creation automatique des table au niveau de la BD

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 import app from "./src/app.js";
 import dotenv from "dotenv";
+import sequelize from "./src/config/sequelize.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, (req, res) => {
   console.log(`Server running on PORT: ${PORT}`);
+  sequelize
+    .sync()
+    .then(() => {
+      console.log("creation des tables avec success");
+    })
+    .catch((err) => {
+      console.log(`Error lors de la creation des table : ${err}`);
+    });
 });
