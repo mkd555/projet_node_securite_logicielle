6bf1f86dfc671e8bef1719b8d78638c24b436337
Mamadou korka Diallo <mamadoukorkad18@gmail.com>
2025-02-04 13:33:55 +0000

    ajout des methodes getClient et getClientByID

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -9,3 +9,22 @@ export const addClient = (req, res) => {
       res.status(404).json({ error: err.message });
     });
 };
+export const getClient = (req, res) => {
+  Client.findAll()
+    .then((client) => {
+      res.status(200).json(client);
+    })
+    .catch((error) => {
+      res.status(404).send(error);
+    });
+};
+export const getClientByID = (req, res) => {
+  const id = req.params.id;
+  Client.findByPk(id)
+    .then((client) => {
+      res.status(200).json(client);
+    })
+    .catch((err) => {
+      res.status(404).send(err);
+    });
+};
